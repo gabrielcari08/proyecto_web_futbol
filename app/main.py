06b329952acbbc42e20136ec4320465b8e0e1882
gabrielcari08 <gabrielcari2008@gmail.com>
2025-06-30 16:17:00 -0300

    WIP: dejamos de momento esta rama debido a que un campo de la tabla user_teams, campo formation, deberia ser Enum

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ app = FastAPI()
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(players.router)
+app.include_router(team.router)
 
 @app.get("/")
 def hello_fastapi():
--- app/routers/team.py
+++ app/routers/team.py
@@ -1,3 +1,8 @@
 from fastapi import APIRouter
 
 router = APIRouter(prefix="/team", tags=["Team"])
+
+#Endpoint de prueba
+@router.get("/")
+async def hello_router():
+    return {"Hello": "Teams!"}
