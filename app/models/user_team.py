2e0dd8d0aa56fa476a3705927ab2458120112f12
gabrielcari08 <gabrielcari2008@gmail.com>
2025-06-30 16:38:44 -0300

    actualizacion en la tabla user_teams en el campo formation, se le agrego Enum

--- app/models/user_team.py
+++ app/models/user_team.py
@@ -1,6 +1,21 @@
-from sqlalchemy import Column, Integer, Float, ForeignKey, String, UniqueConstraint
+from sqlalchemy import Column, Integer, Float, ForeignKey, String, UniqueConstraint, Enum
 from sqlalchemy.orm import declarative_base, relationship
 from app.core.database import Base
+import enum
+
+class FormationEnum(str, enum.Enum):
+    f_4_4_2 = "4-4-2"
+    f_4_3_3 = "4-3-3"
+    f_4_2_3_1 = "4-2-3-1"
+    f_4_3_1_2 = "4-3-1-2"
+    f_4_1_4_1 = "4-1-4-1"
+    f_3_5_2 = "3-5-2"
+    f_5_3_2 = "5-3-2"
+    f_4_4_1_1 = "4-4-1-1"
+    f_4_2_2_2 = "4-2-2-2"
+    f_4_5_1 = "4-5-1"
+    f_3_4_3 = "3-4-3"
+    f_5_4_1 = "5-4-1"
 
 class UserTeam(Base):
     __tablename__ = "user_teams"
@@ -8,7 +23,7 @@ class UserTeam(Base):
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id"))
     matchday_id = Column(Integer, ForeignKey("matchdays.id")) 
-    formation = Column(String, nullable=False)
+    formation = Column(Enum(FormationEnum), nullable=False) #<- Modificamos este campo y lo hacemos Enum
     budget_used = Column(Float, nullable=False)
     captain_id = Column(Integer, ForeignKey("players.id"))
     total_points = Column(Integer, default=0)
